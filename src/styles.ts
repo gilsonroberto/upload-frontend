ff650334256aed3291d1dc949a6087e4a376bd7a
gilson.souza <gilsonroberto@gmail.com>
2022-12-31 17:21:21 -0300

    ajuste scroll

--- src/styles.ts
+++ src/styles.ts
@@ -9,9 +9,15 @@ export const Container = styled.div`
 
 export const Content = styled.div`
     width: 100%;
+    height: 100%;
     max-width: 40rem;
+    max-height: 22rem;
     margin: 18.75rem;
+    overflow-y: auto;
     background: #fff;
     border-radius: 0.25rem;
     padding: 1.25rem;
+    ::-webkit-scrollbar {
+        background: #fff;
+    }
 `;
